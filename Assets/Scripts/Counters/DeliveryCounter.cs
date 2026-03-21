04c1f17cb57e24a5d4baa8aa412d46653bada9fa
yangshulin2333 <yangshulin2333@gmail.com>
2026-03-22 02:48:47 +0800

    交付计数器，着色器图

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+
+
+
+    public override void Interact(Player player)
+    {
+        if(player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
